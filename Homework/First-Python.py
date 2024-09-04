8f9999057b9ddfa5a09d97524251d67c1cb03521
Jamie Kemman <jamieruth@proton.me>
2024-09-04 11:29:50 -0400

    first python assignment

---
+++ Homework/First-Python.py
@@ -0,0 +1,10 @@
+import array
+import random
+random1 = random
+random1.seed(10)
+test1 = random1.randint(1, 10)
+test2 = random1.randint(3,20)
+test3 = random1.randint(5, 40)
+array1 = [test1, test2, test3]
+for x in array1:
+    print(x)
